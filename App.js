72da79b865ae421f30635c1dad5683b570b417d4
Bilal Habib <bilal.habib67@gmail.com>
2021-04-14 18:15:28 +0100

    front end change

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import React from "react";
-// import "./App.css";
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { TextInput, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignupPage from "./Signup";
+import styles from "./style";
+import axios from "axios";
 import { useState } from "react";
-import Axios from "axios";
 
-export default function App() {
+function LoginPage({ navigation }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const addStudent = () => {
-    Axios.post("https://group-project-sql.herokuapp.com/create", {
-      username: username,
-      password: password,
-      headers: { Pragma: "no-cache", "Cache-Control": "no-cache" },
-    })
-      .then(() => {
-        console.log("success");
+  const authenticate = () => {
+    axios
+      .post(
+        "https://group-project-sql.herokuapp.com/create",
+        JSON.stringify({
+          username: username,
+          password: password,
+        })
+      )
+      .then((response) => {
+        console.log("Connected to sql backend");
       })
       .catch((err) => {
         console.log(err);
@@ -22,24 +30,49 @@ export default function App() {
   };
 
   return (
-    <div className="App">
-      <div className="information">
-        <label>Name:</label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setUsername(event.target.value);
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={(text) => setUsername(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        onChangeText={(text) => setPassword(text)}
+      />
+
+      <Button title="Log In" onPress={authenticate} />
+      <Button title="Signup" onPress={() => navigation.navigate("Signup")} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginPage}
+          options={{
+            headerTitleStyle: { alignSelf: "center" },
+            title: "Log In To Your Account",
           }}
         />
-        <label>Age:</label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setPassword(event.target.value);
+        <Stack.Screen
+          name="Signup"
+          component={SignupPage}
+          options={{
+            headerTitleStyle: { alignSelf: "center" },
+            title: "Sign Up",
           }}
         />
-        <button onClick={addStudent}> Add </button>
-      </div>
-    </div>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
