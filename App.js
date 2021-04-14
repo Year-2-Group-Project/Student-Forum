78dc4ffbf215aadec63b82f5aade182b746b1641
Shiraz7800 <56200497+Shiraz7800@users.noreply.github.com>
2021-04-14 16:30:22 +0100

    Update App.js
    Frontend test

--- App.js
+++ App.js
@@ -1,78 +1,46 @@
-import * as React from "react";
-import { StatusBar } from "expo-status-bar";
-import { TextInput, View, Button } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import SignupPage from "./Signup";
-import styles from "./style";
-import axios from "axios";
+import React from 'react';
+import "./App.css";
 import { useState } from "react";
+import Axios from 'axios'
 
-function LoginPage({ navigation }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
 
-  const authenticate = () => {
-    axios
-      .post(
-        "https://dashboard.heroku.com/apps/mysql-year2-proj/",
-        JSON.stringify({
-          username: username,
-          password: password,
-        })
-      )
-      .then((response) => {
-        console.log("Connected to sql backend");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        onChangeText={(text) => setUsername(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry={true}
-        onChangeText={(text) => setPassword(text)}
-      />
-
-      <Button title="Log In" onPress={() => authenticate()} />
-      <Button title="Signup" onPress={() => navigation.navigate("Signup")} />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
 
 export default function App() {
+  const [name , setName] = useState("");
+  const [age , setAge] = useState("");
+  
+
+  const addStudent = () => {
+    Axios.post('https://group-project-sql.herokuapp.com/create', {
+      name: name,
+      age: age,
+      headers: { Pragma: 'no-cache', 'Cache-Control': 'no-cache' },
+    }).then(() => {
+      console.log("success");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginPage}
-          options={{
-            headerTitleStyle: { alignSelf: "center" },
-            title: "Log In To Your Account",
-          }}
+    <div className="App">
+      <div className = "information">
+        <label>Name:</label>
+        <input 
+        type="text" 
+        onChange={(event) => {setName(event.target.value)}} 
         />
-        <Stack.Screen
-          name="Signup"
-          component={SignupPage}
-          options={{
-            headerTitleStyle: { alignSelf: "center" },
-            title: "Sign Up",
-          }}
+        <label>Age:</label>
+        <input 
+        type="text"
+        onChange={(event) => {setAge(event.target.value)}}
         />
-      </Stack.Navigator>
-    </NavigationContainer>
+        <button onClick={addStudent}> Add </button>
+      </div>
+    </div> 
   );
 }
+
+
