50bd7d37693fa1f8d6a26f6b85b17b47bc747ae8
Bilal Habib <bilal.habib67@gmail.com>
2021-04-14 17:49:01 +0100

    revert to html code

--- App.js
+++ App.js
@@ -1,75 +1,25 @@
-import * as React from "react";
-import { StatusBar } from "expo-status-bar";
-import { TextInput, View, Button } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import SignupPage from "./Signup";
-import styles from "./style";
-import axios from "axios";
+import React from "react";
+// import "./App.css";
 import { useState } from "react";
+import Axios from "axios";
 
-function LoginPage({ navigation }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+export default function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
 
-  // const authenticate = () => {
-  //   axios
-  //     .post(
-  //       "https://group-project-sql.herokuapp.com/create",
-  //       JSON.stringify({
-  //         username: username,
-  //         password: password,
-  //       })
-  //     )
-  //     .then((response) => {
-  //       console.log("Connected to sql backend");
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
-
-  const authenticate = () => {
-    console.log("button pressed");
-    axios
-      .post("https://group-project-sql.herokuapp.com/create", {
-        username: username,
-        password: password,
-        headers: { Pragma: "no-cache", "Cache-Control": "no-cache" },
-      })
+  const addStudent = () => {
+    Axios.post("https://group-project-sql.herokuapp.com/create", {
+      name: name,
+      age: age,
+      headers: { Pragma: "no-cache", "Cache-Control": "no-cache" },
+    })
       .then(() => {
         console.log("success");
       })
       .catch((err) => {
         console.log(err);
       });
-    console.log(username);
-    console.log(password);
   };
-
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        onChange={(event) => {
-          setUsername(event.target.value);
-        }}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry={true}
-        onChange={(event) => {
-          setPassword(event.target.value);
-        }}
-      />
-
-      <Button title="Log In" onPress={() => authenticate} />
-      <Button title="Signup" onPress={() => navigation.navigate("Signup")} />
-      <StatusBar style="auto" />
-    </View>
-  );
 
   return (
     <div className="App">
@@ -91,32 +41,5 @@ function LoginPage({ navigation }) {
         <button onClick={addStudent}> Add </button>
       </div>
     </div>
-  );
-}
-
-const Stack = createStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginPage}
-          options={{
-            headerTitleStyle: { alignSelf: "center" },
-            title: "Log In To Your Account",
-          }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={SignupPage}
-          options={{
-            headerTitleStyle: { alignSelf: "center" },
-            title: "Sign Up",
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
   );
 }
