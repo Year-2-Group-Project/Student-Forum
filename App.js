6bf7182fa9d9a0c1bd8d6345613b30ef3cd84230
Bilal Habib <bilal.habib67@gmail.com>
2021-04-14 17:27:37 +0100

    front end change

--- App.js
+++ App.js
@@ -12,17 +12,32 @@ function LoginPage({ navigation }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  // const authenticate = () => {
+  //   axios
+  //     .post(
+  //       "https://group-project-sql.herokuapp.com/create",
+  //       JSON.stringify({
+  //         username: username,
+  //         password: password,
+  //       })
+  //     )
+  //     .then((response) => {
+  //       console.log("Connected to sql backend");
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
   const authenticate = () => {
     axios
-      .post(
-        "https://group-project-sql.herokuapp.com/create",
-        JSON.stringify({
-          username: username,
-          password: password,
-        })
-      )
-      .then((response) => {
-        console.log("Connected to sql backend");
+      .post("https://group-project-sql.herokuapp.com/create", {
+        username: username,
+        password: password,
+        headers: { Pragma: "no-cache", "Cache-Control": "no-cache" },
+      })
+      .then(() => {
+        console.log("success");
       })
       .catch((err) => {
         console.log(err);
